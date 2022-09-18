a5a1b8995cad57fd71e2517ad2ffe5a600476b0d
he1ex-tG <gosu.he1ex@gmail.com>
2022-09-18 20:05:16 +0500

    Added PostgresSQL database connection

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,6 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
@@ -27,6 +26,9 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 	implementation("org.springframework.boot:spring-boot-starter-security")
+
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.postgresql:postgresql")
 }
 
 tasks.withType<KotlinCompile> {
