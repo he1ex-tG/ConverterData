792520b82cbd19100db742a324ae1338d7afd05d
he1ex-tG <gosu.he1ex@gmail.com>
2023-02-21 23:36:53 +0500

    Rework UserDTO, now it contains list of files

--- src/main/kotlin/com/he1extg/converterdata/dao/user/UserDAO.kt
+++ src/main/kotlin/com/he1extg/converterdata/dao/user/UserDAO.kt
@@ -1,11 +1,6 @@
 package com.he1extg.converterdata.dao.user
 
-import com.he1extg.converterdata.dto.user.UserDTO
-
 data class UserDAO(
     val id: Long,
     val username: String
-) {
-
-    fun toUserDTO(): UserDTO = UserDTO(id, username)
-}
+)
--- src/main/kotlin/com/he1extg/converterdata/dto/user/UserDTO.kt
+++ src/main/kotlin/com/he1extg/converterdata/dto/user/UserDTO.kt
@@ -1,6 +1,9 @@
 package com.he1extg.converterdata.dto.user
 
+import com.he1extg.converterdata.dto.file.FilenameDTO
+
 class UserDTO(
     val id: Long,
-    val username: String
+    val username: String,
+    val files: List<FilenameDTO>
 )
